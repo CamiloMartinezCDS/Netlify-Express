9dfb66ba4e3f8c726bfe9ce931411116eb67120f
Camilo <camilo.nicolas.martinez@gmail.com>
2022-07-27 15:52:19 -0300

    Added include to static assets

--- src/express.js
+++ src/express.js
@@ -8,46 +8,47 @@ const config = require('./config');
 
 const app = express();
 const router = express.Router();
-const views = path.resolve('./views/');
 
 // Video page route.
-router.get('/:videoId',async (req, res) => {
-  try {
-    const videoId = req.params.videoId;
-    if (!videoId) {
-      res.redirect('http://immi.io');
-    } else {
-      const video = await axios.get(`${config.API_URL}/user-generated-content/${videoId}`, { headers: { 'Authorization': `APIKEY ${config.API_KEY}` } });
-      if (video) {
-        const { id, uid } = video.data
-        const videoView = pug.renderFile(`${views}/videoPage.pug`, { id, uid });
-        console.log('VideoView => ', videoView);
-        res.set('Content-Type', 'text/html');
-        res.send(Buffer.from(videoView));
-      } else {
-        res.redirect('http://immi.io');
-      }
+router.get('/:videoId', async (req, res) => {
+    try {
+        const videoId = req.params.videoId;
+        if (!videoId) {
+            res.redirect('http://immi.io');
+        } else {
+            const videoViewRoute = path.resolve('./src/views/videoPage.pug');
+            console.log('videoViewRoute => ', videoViewRoute)
+            const video = await axios.get(`${config.API_URL}/user-generated-content/${videoId}`, { headers: { 'Authorization': `APIKEY ${config.API_KEY}` } });
+            if (video) {
+                const { id, uid } = video.data
+                const videoView = pug.renderFile(videoViewRoute, { id, uid });
+                console.log('VideoView => ', videoView);
+                res.set('Content-Type', 'text/html');
+                res.send(Buffer.from(videoView));
+            } else {
+                res.redirect('http://immi.io');
+            }
+        }
+    } catch (error) {
+        console.log('Error => ', error);
+        res.send('Failed to render video page').statusCode(500);
     }
-  } catch (error) {
-    console.log('Error => ', error);
-    res.send('Failed to render video page').statusCode(500);
-  }
 });
 
 // Home route.
 router.get('/', (req, res) => {
-  try {
-    console.log('views => ', views)
-    const viewLocation = `${views}/index.pug`;
-    console.log('ViewLocation => ', viewLocation);
-    const defaultView = pug.renderFile(viewLocation);
-    console.log('defaultView => ', defaultView);
-    res.set('Content-Type', 'text/html');
-    res.send(Buffer.from(defaultView));
-  } catch (error) {
-    console.log('Error => ', error);
-    res.send('Failed to render defaut view').statusCode(500);
-  }
+    try {
+        console.log('views => ', views)
+        const viewLocation = `${views}/index.pug`;
+        console.log('ViewLocation => ', viewLocation);
+        const defaultView = pug.renderFile(viewLocation);
+        console.log('defaultView => ', defaultView);
+        res.set('Content-Type', 'text/html');
+        res.send(Buffer.from(defaultView));
+    } catch (error) {
+        console.log('Error => ', error);
+        res.send('Failed to render defaut view').statusCode(500);
+    }
 });
 
 app.use(`/.netlify/functions/express`, router);
